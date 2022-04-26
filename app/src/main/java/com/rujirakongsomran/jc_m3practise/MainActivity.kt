0ad40b8f0a6f41df9c945ba2b3c49acd09312e94
RujiraKongsomran <rujirabiwberry@gmail.com>
2022-04-26 22:11:14 +0700

    Infinite Animation

--- app/src/main/java/com/rujirakongsomran/jc_m3practise/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_m3practise/MainActivity.kt
@@ -3,7 +3,9 @@ package com.rujirakongsomran.jc_m3practise
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
@@ -12,6 +14,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -27,11 +30,27 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ToggleableSurface()
+                    InfiniteAnimation()
                 }
             }
         }
     }
+}
+
+@Composable
+fun InfiniteAnimation() {
+    val infiniteTransition = rememberInfiniteTransition()
+    val color by infiniteTransition.animateColor(
+        initialValue = Color.Green,
+        targetValue = Color.Blue,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(color = color))
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
